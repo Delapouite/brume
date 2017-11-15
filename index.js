2d7148a0ef52e174ebdf47a29cdf1fea3208de56
Delapouite <delapouite@gmail.com>
2017-11-15 08:19:54 +0100

    refactor: remove cheerio

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request')
 
-const URL = 'http://www.radiobrume.fr/data.php';
+const URL = 'http://www.radiobrume.fr/data.php'
 
-module.exports = function getInfo () {
-	return new Promise((resolve, reject) => {
-		request(URL, (err, res, html) => {
-			if (err) reject(err);
+module.exports = function getInfo() {
+  return new Promise((resolve, reject) => {
+    request(URL, (err, res, html) => {
+      if (err) reject(err)
 
-			var $ = cheerio.load(html);
-			resolve({
-				artist: $('tr td:nth-child(2)').text().trim(),
-				title: $('tr td:nth-child(3)').text().trim()
-			});
-		});
-	});
+      const regexp = /<td.*?>(.*?)<\/td>/g
+      let arr
+      while ((arr = regexp.exec(html)) !== null) {
+        if (!arr[1] || arr[1].includes('On air')) continue
+        console.log(arr[1])
+      }
+    })
+  })
 }
